9619bd03eebb90112843c17a93c83272a69e012e
Dominique Youness <youness.dominique@gmail.com>
2022-06-22 12:14:21 +0200

    [FEAT] Login Component config and add dialog register

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from "@angular/forms";
+import {RegisterComponent} from "../register/register.component";
+import {Subscription} from "rxjs";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-login',
@@ -7,17 +13,55 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  subs: Subscription[] = [];
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth,
+              private authService: AuthService,
+              private router: Router,
+              private matDialog: MatDialog) {
+  }
 
   ngOnInit(): void {
+    this.subs.push(this.authService.userData.subscribe(data => {
+      if (data) {
+        this.router.navigateByUrl('/').then();
+      }
+    }));
+
+
+  }
+
+  ngOnDestroy(): void {
+    this.subs.map(s => s.unsubscribe());
   }
 
   login(form: NgForm): void {
+    const {email, password} = form.value;
 
+
+    if (!form.valid) {
+      return;
+    }
+    this.authService.SignIn(email, password);
+    form.resetForm();
   }
 
   openRegister(): void {
+    const dialogRef = this.matDialog.open(RegisterComponent, {
+      role: 'dialog',
+      height: '480px',
+      width: '480px'
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      const {fname, lname, email, password, avatar} = result;
+
+      if (result !== undefined) {
+        this.authService.SignUp(email, password, fname, lname, avatar);
+      }
+
+      return;
+    });
   }
+
 }
