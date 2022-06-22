36f08f2d158719446fa7b6bdd736cba4a1d38910
Dominique Youness <youness.dominique@gmail.com>
2022-06-22 12:17:16 +0200

    [FEAT] Config Guard

--- src/app/guards/facebook.guard.ts
+++ src/app/guards/facebook.guard.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {AuthService} from "../services/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacebookGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private router: Router,
+              private afAuth: AngularFireAuth,
+              private authService: AuthService) {
   }
-  
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.userData
+      .pipe(
+        map(user => user !== null),
+        tap(value => {
+          if (!value) {
+            this.router.navigateByUrl('/login').then();
+            return value;
+          } else {
+            return value;
+          }
+        })
+      );
+  }
+
 }
